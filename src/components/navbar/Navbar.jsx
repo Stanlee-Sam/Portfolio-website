d1b0a099e8244b426e9542f6f17c1d80a49a88bf
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2024-08-07 16:53:03 +0300

    Sidebar commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,21 @@
-import './navbar.scss'
+import Sidebar from '../sidebar/Sidebar';
+import './navbar.scss';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
   return (
     <div className = "navbar">
-      {/* sidebar */}
       <div className="wrapper">
-        <span>Stanlee-Sam</span>
+        <motion.span 
+        initial = {{ opacity : 0, scale : 0.5}}
+        animate = {{ opacity : 1, scale : 1}}
+        transition = {{ duration : 0.5}}
+        >Stanlee-Sam
+        </motion.span>
+
+      </div>
+      <div className="side">
+      <Sidebar/>
 
       </div>
     </div>
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,40 @@
+import "./sidebar.scss";
+import Links from "./links/Links";
+import ToggleButton from "./toggleButton/ToggleButton.jsx";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+const variants = {
+    open: {
+      clipPath: "circle(1500px at 87% 8%)",
+      transition: {
+        type: "spring",
+        stiffness: 20,
+      },
+    },
+    closed: {
+      clipPath: "circle(30px at 87% 8%)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+
+  
+  return (
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <ToggleButton setOpen={setOpen} />
+      <motion.div className="bg" variants={variants}>
+        <Links />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Sidebar;
