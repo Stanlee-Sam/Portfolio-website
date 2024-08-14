97ddee3aacac6db93c7dd8ff5ff3542bf48523cc
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2024-08-14 11:43:33 +0300

    Services section animation

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,31 +1,47 @@
 import "./services.scss";
-import { motion } from "framer-motion";
+import {  motion } from "framer-motion";
 
+const variants = {
+  initial : {
+    x : -500,
+    y : 100,
+    opacity: 0,
+  },
+  animate : {
+    x : 0,
+    opacity : 1,
+    y : 0,
+    transition : {
+      duration : 1,
+      staggerChildren : 0.1
+    }
+  }
+}
 const Services = () => {
   return (
-    <motion.div className="services">
-      <motion.div className="textContainer">
+    <motion.div className="services" variants = {variants} initial = "initial" whileInView = "animate">
+      <motion.div className="textContainer" variants = {variants}>
         <p>
           I focus on making your brand grow
           <br /> and move forward
         </p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants = {variants}>
         <div className="title">
           <img src="./People2.jpeg" alt="" />
           <h1>
-            <b className="bold">Unique</b> Ideas
+            <motion.b className="bold" whileHover = {{color:"Orange"}}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className="title">
           <h1>
-            <b className="bold">For Your</b> Business.
+            <motion.b className="bold"whileHover = {{color:"Orange"}}>For Your</motion.b> Business.
           </h1>
           <button> What I Do?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
+      <motion.div className="listContainer" variants = {variants}>
         <motion.div
           className="box"
           whileHover={{ background: "lightgray", color: "black" }}
