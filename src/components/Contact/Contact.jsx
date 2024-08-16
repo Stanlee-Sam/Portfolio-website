c6c5f4907ddb739248595d0ef2bee2973f112ce2
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2024-08-16 17:01:30 +0300

    Contact section complete

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,29 @@
 import "./contact.scss";
+import emailjs from "@emailjs/browser";
+import { useState, useRef } from "react";
 
 const Contact = () => {
+  const form = useRef();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_h5kg4dc", "template_jsh4lol", form.current, {
+        publicKey: "kNYDeQd-speujsVrw",
+      })
+      .then(
+        () => {
+          setSuccess(true);
+        },
+        (error) => {
+          setError(true);
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <div className="contact">
       <div className="textContainer">
@@ -19,17 +42,20 @@ const Contact = () => {
         </div>
       </div>
       <div className="formContainer">
-        <form action="">
-            <input type="text" required placeholder="Name" />
-            <input type="email" required placeholder="Email" />
-            <textarea rows = {0} placeholder="Message" />
+        <div className="formContainer">
+          <form ref = {form} action="" onSubmit={sendEmail}>
+            <input type="text" required placeholder="Name" name="name" />
+            <input type="email" required placeholder="Email" name="email" />
+            <textarea rows={0} placeholder="Message" name="message" />
             <button type="submit">Send Message</button>
-           
-
-          <div className="error-message"></div>
-
-        </form>
-
+            {error && (
+              <span className="error-message">Error sending email</span>
+            )}
+            {success && (
+              <span className="success-message">Email sent successfully</span>
+            )}
+          </form>
+        </div>
       </div>
     </div>
   );
